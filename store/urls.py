ad5e61ede7a5c271ef8b2166678dbbb5c913c95f
Vigneswar-A <ugiv.rawsengiv@gmail.com>
2023-11-26 19:49:54 +0530

    git ignored

--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from store import views
-from django.conf.urls.static import static
-from CarsNBikes import settings
-urlpatterns = [
-    path("", views.home),
-    path("sell/", views.sell),
-    path("buy/",views.buy)
-] + static(settings.STATIC_URL)
